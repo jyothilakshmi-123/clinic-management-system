7b2561c351f59e76a8b83c58a2b420ddff99a5ca
Jyothilakshmi C <jyothilakshmi1491@gmail.com>
2020-11-25 19:35:09 +0530

    admin login created with bcrypted password

--- helpers/admin-helpers.js
+++ helpers/admin-helpers.js
@@ -3,28 +3,43 @@ var collection = require('../config/collections')
 const bcrypt = require('bcrypt')
 
 module.exports={
+    // let adminUpdate = {}
+
+    
     doAdminLogin:(adminData)=>{
         console.log(adminData)
         return new Promise(async(resolve,reject)=>{
-            // let loginStatus = false
+            let loginStatus = false
             let response = {}
-            let admin = await db.get().collection(collection.ADMIN_COLLECTION).findOne({Email:adminData.Email,Password:adminData.Password})
+            let admin = await db.get().collection(collection.ADMIN_COLLECTION).findOne({Email:adminData.Email})
             
-            // let passwordHash = bcrypt.hash(admin.Password,10)
+            
             // console.log("db pw-----------" + admin.Password)
             // console.log("web pw-----------" + adminData.Password)
+            let newPassword =await bcrypt.hash(admin.Password,10)
+            // console.log("bcrypted..." +newPassword)
+            // let adminDetails = await db.get().collection(collection.ADMIN_COLLECTION).updateOne({Password:"abcd"},{$set:{Password:newPassword}})
+
             if(admin){
-                
+                bcrypt.compare(adminData.Password,admin.Password).then((status)=>{
+                    if(status){
+
                         console.log("Login Success")
                         response.admin = admin
                         response.status = true
                         resolve(response)
                     }
-                    
-            else{
-                console.log("login failed")
+                    else{
+                        console.log("login failed")
                 resolve({status:false})
+                    }
+                })
             }
+            else{
+                    console.log("login failed")
+                    resolve({status:false})
+                }
+                
         })
     },
     
