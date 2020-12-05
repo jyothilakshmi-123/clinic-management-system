bb4f7ab612b1c18916555eb7303ce2352c8d229f
Jyothilakshmi C <jyothilakshmi1491@gmail.com>
2020-12-05 14:51:23 +0530

    dr from database is now in home page and some design modified

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const doctorHelpers = require('../helpers/doctor-helpers');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('user/user-home' , {user:true});
+  let doctorsList=  doctorHelpers.getActiveDoctorsList().then((doctorsList)=>{
+    console.log(doctorsList)
+ 
+  res.render('user/user-home' , {user:true, doctorsList});
+})
 });
 
 module.exports = router;
